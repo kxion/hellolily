596ac8c1f06135890c574c43604a9ba47a517b2b
Hans Adema <hans.adema@wearespindle.com>
2017-11-10 14:24:15 +0100

    LILY-2619: Refactor search_index command to use aliases.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && python manage.py test --with-coverage --cover-erase --cover-package=lily"
+docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && Dockers/wait-for es:9200 && coverage run manage.py search_index create && python manage.py search_index autoalias && python manage.py test --with-coverage --cover-erase --cover-package=lily"
