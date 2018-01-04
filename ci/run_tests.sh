56914513ad1103a28c79466d44ee3608be1784d2
Jukka Koivunen <jjkoivun@gmail.com>
2018-01-04 14:48:56 +0100

    LILY-2820: Disable debug mode since that option is only django 1.11 or newer.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && python manage.py test --parallel --debug-mode"
+docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && python manage.py test --parallel"
