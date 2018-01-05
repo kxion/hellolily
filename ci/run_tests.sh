addf7ea0e73932347b1e38795bcabdc8c53e69a0
Jukka Koivunen <jjkoivun@gmail.com>
2018-01-05 15:44:13 +0100

    LILY-2820: Disable verbose mode.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && python manage.py test --parallel --verbosity=2"
+docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && python manage.py test --parallel"
